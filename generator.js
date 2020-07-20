081abf72f204291b7319414d893401fca8a226e7
ALICARMONA <afacm94@gmail.com>
2020-07-20 18:54:51 +0000

    finalizado

--- generator.js
+++ generator.js
@@ -1,11 +1,11 @@
-var pronoun = ['the','our']; 
-var adj = ['great', 'big' ]; 
-var noun = ['jogger','racoon'];
+let pronoun = ['el','la']; 
+let adj = ['verde', 'gigante' ]; 
+let noun = ['cosa','carro'];
 
 for (i=0; i<pronoun.length; i++){
 	for(j=0; j<adj.length; j++){
 		for(k=0; k<noun.length; k++){
-			document.write(pronoun[i] + " "+adj[j] + " "+noun [k] + "\n");
+			console.log(pronoun[i] + " "+adj[j] + " "+noun [k] );
 		}
 	}
 }
